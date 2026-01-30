6a79d1f827d89ffca73eb1e51d11634f3f817b0f
Luca Giampieri <luca.giampieri95@gmail.com>
2026-01-30 16:01:44 +0100

    Spostamento in BudgetContext dell'API dei prodotti e della rripettiva chiamta Ajax

--- src/contexts/BudgetContext.jsx
+++ src/contexts/BudgetContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const BudgetContext = createContext();
 
@@ -10,12 +11,31 @@ function BudgetProvider({ children }) {
         return budgetMode ? (setBudgetMode(false)) : (setBudgetMode(true))
     }
 
+    //creo una variabile in cui salvare l'API
+    const enpoint = "https://fakestoreapi.com/products";
+
+    //creo una variabile in cui inserire la lista prodotti
+    const [productsList, setProcudtsList] = useState([]);
+
+    //creiamo una funzione per estrapolare i dati dall'API 
+    function fetchProducts() {
+        axios.get(enpoint)
+            .then((res) => setProcudtsList(res.data))
+            .catch(err => console.error("Errore nella richiesta"))
+    }
+
+    //andiamo a richiamre la funzione una sola volta all'avvio
+    useEffect(fetchProducts, []);
+
     return (
         <BudgetContext.Provider
             value={{
                 budgetMode,
+                productsList,
+                setProcudtsList,
                 setBudgetMode,
-                buttonToggle
+                buttonToggle,
+                fetchProducts
             }}>
             {children}
         </BudgetContext.Provider>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,28 +1,11 @@
-import { useEffect, useState } from "react"
-import axios from "axios";
 import CardProduct from "../products/CardProduct";
 import { useBudgetMode } from "../contexts/BudgetContext";
 
 function Products() {
 
     // usiamo il contesto, richiamando solo i valori che ci servono qui
-    const { budgetMode } = useBudgetMode();
+    const { budgetMode, productsList } = useBudgetMode();
 
-    //creo una variabile in cui salvare l'API
-    const enpoint = "https://fakestoreapi.com/products";
-
-    //creo una variabile in cui inserire la lista prodotti
-    const [productsList, setProcudtsList] = useState([]);
-
-    //creiamo una funzione per estrapolare i dati dall'API 
-    function fetchProducts() {
-        axios.get(enpoint)
-            .then((res) => setProcudtsList(res.data))
-            .catch(err => console.error("Errore nella richiesta"))
-    }
-
-    //andiamo a richiamre la funzione una sola volta all'avvio
-    useEffect(fetchProducts, []);
 
     return (
         <>
